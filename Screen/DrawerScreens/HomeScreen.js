c5a8a852c38776a3dd8257b6651abe5a345136ec
Kavya Lokuge <82757644+kavyaLokuge@users.noreply.github.com>
2022-10-27 14:17:08 +0530

    initial project

---
+++ Screen/DrawerScreens/HomeScreen.js
@@ -0,0 +1,47 @@
+// Import React and Component
+import React from 'react';
+import {View, Text, SafeAreaView} from 'react-native';
+
+const HomeScreen = () => {
+  return (
+    <SafeAreaView style={{flex: 1}}>
+      <View style={{flex: 1, padding: 16}}>
+        <View
+          style={{
+            flex: 1,
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}>
+          <Text
+            style={{
+              fontSize: 20,
+              textAlign: 'center',
+              marginBottom: 16,
+            }}>
+            SoundCores - Sound Type Detection App
+            {'\n\n'}
+            Home Screen
+          </Text>
+        </View>
+        <Text
+          style={{
+            fontSize: 18,
+            textAlign: 'center',
+            color: 'grey',
+          }}>
+          Sound type recognition{'\n'}React Native
+        </Text>
+        <Text
+          style={{
+            fontSize: 16,
+            textAlign: 'center',
+            color: 'grey',
+          }}>
+          SoundCores v 1.0.0
+        </Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default HomeScreen;
