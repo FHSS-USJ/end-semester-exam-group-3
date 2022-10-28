d8f13f53bcd807058ff7eb367f7c26fc8fa7c879
Kavya Lokuge <82757644+kavyaLokuge@users.noreply.github.com>
2022-10-28 15:15:24 +0530

    sidebar changed to white theme

--- Screen/Components/CustomSidebarMenu.js
+++ Screen/Components/CustomSidebarMenu.js
@@ -11,7 +11,7 @@ import {
 
 import AsyncStorage from '@react-native-community/async-storage';
 
-const CustomSidebarMenu = (props) => {
+const CustomSidebarMenu = props => {
   return (
     <View style={stylesSidebar.sideMenuContainer}>
       <View style={stylesSidebar.profileHeader}>
@@ -27,7 +27,7 @@ const CustomSidebarMenu = (props) => {
       <DrawerContentScrollView {...props}>
         <DrawerItemList {...props} />
         <DrawerItem
-          label={({color}) => <Text style={{color: 'white'}}>Logout</Text>}
+          label={({color}) => <Text style={{color: '#1F319D'}}>Logout</Text>}
           onPress={() => {
             props.navigation.toggleDrawer();
             Alert.alert(
@@ -63,7 +63,7 @@ const stylesSidebar = StyleSheet.create({
   sideMenuContainer: {
     width: '100%',
     height: '100%',
-    backgroundColor: '#1F319D',
+    backgroundColor: 'white',
     paddingTop: 40,
     color: 'white',
   },
