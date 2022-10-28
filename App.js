5cd15b90e30d1de3748bd6826a5238859f15a4c7
Kavya Lokuge <82757644+kavyaLokuge@users.noreply.github.com>
2022-10-28 20:05:04 +0530

    generated APK

--- App.js
+++ App.js
@@ -30,7 +30,7 @@ const Auth = () => {
         options={{
           title: 'Register', //Set Header Title
           headerStyle: {
-            backgroundColor: '#307ecc', //Set Header color
+            backgroundColor: '#1F319D', //Set Header color
           },
           headerTintColor: '#fff', //Set Header text color
           headerTitleStyle: {
