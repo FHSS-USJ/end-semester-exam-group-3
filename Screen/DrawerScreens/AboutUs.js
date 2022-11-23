08c41253a11bfba3ee2e576abf6bc2b62c97be26
Nipuni Cabral <68634390+cabral0413@users.noreply.github.com>
2022-11-23 15:00:48 +0530

    Adding styles to the about us screen

--- Screen/DrawerScreens/AboutUs.js
+++ Screen/DrawerScreens/AboutUs.js
@@ -95,3 +95,94 @@ import {
     </ScrollView>
   );
 };
+
+
+const styles = StyleSheet.create({
+    aboutContainer: {
+      display: 'flex',
+      alignItems: 'center',
+    },
+  
+    imgStyle: {
+      width: 150,
+      height: 150,
+      borderRadius: 100,
+    },
+    MainTitle: {
+      fontSize: 20,
+      color: '#000000',
+      fontWeight: 'bold',
+      fontStyle: 'italic',
+      textTransform: 'uppercase',
+      marginTop: 50,
+      alignSelf: 'flex-start',
+      marginStart: 10,
+      textShadowColor: 'blue',
+      textShadowRadius: 6,
+      fontFamily: 'serif',
+      lineHeight: 40,
+    },
+    mainHeader: {
+      fontSize: 16,
+      color: '#000',
+      textTransform: 'uppercase',
+      marginTop: 50,
+      marginBottom: 10,
+      textAlign: 'center',
+      fontFamily: 'Roboto',
+      fontWeight: 'bold',
+    },
+    paraStyle: {
+      fontSize: 18,
+      color: '#7d7d7d',
+      paddingBottom: 30,
+      fontFamily: 'sans-serif-medium',
+    },
+    aboutLayout: {
+      backgroundColor: '#1F319D',
+      paddingHorizontal: 30,
+      marginVertical: 30,
+      marginHorizontal: 10,
+      marginTop: 30,
+      borderRadius: 50,
+    },
+    aboutSubHeader: {
+      fontSize: 18,
+      color: '#fff',
+      textTransform: 'uppercase',
+      fontWeight: '500',
+      marginVertical: 15,
+      fontFamily: 'JosefinSans_700Bold',
+      alignSelf: 'center',
+    },
+    aboutPara: {
+      color: '#fff',
+      marginTop: 30,
+      textAlign: 'justify',
+    },
+    menuContainer: {
+      width: '100%',
+      flexDirection: 'row',
+      justifyContent: 'space-evenly',
+      marginTop: 25,
+      marginBottom: 30,
+    },
+    Images: {
+      flexDirection: 'column',
+    },
+  
+    iconStyle: {
+      width: '100%',
+      height: 50,
+      aspectRatio: 1,
+    },
+    bottom: {
+      fontSize: 16,
+      textAlign: 'center',
+      color: 'grey',
+      marginBottom: 30,
+    },
+});
+  
+export default App;
+  
