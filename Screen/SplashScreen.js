e8074c89c05e90f6b08e0ad315d59aa6aa5813fe
Nipuni Cabral <68634390+cabral0413@users.noreply.github.com>
2022-11-27 16:41:58 +0530

    adding firebase to the splash screen

--- Screen/SplashScreen.js
+++ Screen/SplashScreen.js
@@ -1,8 +1,9 @@
+
 // Import React and Component
 import React, {useState, useEffect} from 'react';
 import {ActivityIndicator, View, StyleSheet, Image} from 'react-native';
 
-import AsyncStorage from '@react-native-community/async-storage';
+import auth from '@react-native-firebase/auth';
 
 const SplashScreen = ({navigation}) => {
   //State for ActivityIndicator animation
@@ -14,10 +15,8 @@ const SplashScreen = ({navigation}) => {
       //Check if user_id is set or not
       //If not then send for Authentication
       //else send to Home Screen
-      AsyncStorage.getItem('user_id').then(value =>
-        navigation.replace(value === null ? 'Auth' : 'DrawerNavigationRoutes'),
-      );
-    }, 5000);
+      navigation.replace(auth().currentUser ? 'HomeScreen' : 'Auth');
+    }, 1000);
   }, []);
 
   return (
@@ -50,3 +49,4 @@ const styles = StyleSheet.create({
     height: 80,
   },
 });
+
